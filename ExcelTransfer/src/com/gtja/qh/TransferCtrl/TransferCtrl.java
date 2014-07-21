0f4f76e9f15c3f89c877bd4d89885564e6aeb1ef
wangnantu <wangnantu@hotmail.com>
2014-07-21 23:21:36 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	ExcelTransfer/nbproject/project.properties
    	ExcelTransfer/src/com/gtja/qh/Transfer.java

--- ExcelTransfer/src/com/gtja/qh/TransferCtrl/TransferCtrl.java
+++ ExcelTransfer/src/com/gtja/qh/TransferCtrl/TransferCtrl.java
@@ -17,8 +17,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -87,15 +85,14 @@ public class TransferCtrl {
           });
     }
     
-     private void generateButtonActionPerformed(java.awt.event.ActionEvent evt) throws DBFException {                                         
-        String date = frame.getDate().getText();
+     private void generateButtonActionPerformed(java.awt.event.ActionEvent evt) throws DBFException { 
         String inputFilePath = frame.getFilePath().getText();
-        String fileName;
+      if(inputFilePath.length() != 0 ){ 
+        String date = frame.getDate().getText();
         if(!(frame.getTxtCB().isSelected()||frame.getDbfCB().isSelected())){
              JOptionPane.showMessageDialog(null, "请至少选择一种文件类型", "注意", JOptionPane.ERROR_MESSAGE);
         }else{
             if(isValidDate(date)){
-                 fileName = "0004_00000001_" + date + "_DailyFundChg";
                 JFileChooser chooser = new JFileChooser();
                  chooser.setDialogTitle("请选择导出目录");
                  chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
@@ -105,10 +102,10 @@ public class TransferCtrl {
                     Boolean t = false;
                     Boolean d = false;
                     if(frame.getTxtCB().isSelected()){
-                        t = transferToTxt(inputFilePath,fileName,outFileDir);
+                        t = transferToTxt(inputFilePath,outFileDir);
                         if(!t){
                             if(frame.getDbfCB().isSelected()){
-                               d = transferToDbf(inputFilePath,fileName,outFileDir);   
+                               d = transferToDbf(inputFilePath,outFileDir);   
                                 if(!d){
                                 JOptionPane.showMessageDialog(null, "文件生成失败", "注意", JOptionPane.ERROR_MESSAGE);
                                 }else{
@@ -117,7 +114,7 @@ public class TransferCtrl {
                             }
                         }else{
                         if(frame.getDbfCB().isSelected()){
-                         d = transferToDbf(inputFilePath,fileName,outFileDir); 
+                         d = transferToDbf(inputFilePath,outFileDir); 
                             
                             if(!d){
                                 JOptionPane.showMessageDialog(null, "Txt文件生成成功，Dbf文件生成失败", "注意", JOptionPane.ERROR_MESSAGE);
@@ -129,7 +126,7 @@ public class TransferCtrl {
                     }
                    }
                  }else{
-                         d = transferToDbf(inputFilePath,fileName,outFileDir);
+                         d = transferToDbf(inputFilePath,outFileDir);
                  
                          if(!d){
                                 JOptionPane.showMessageDialog(null, "Dbf文件生成失败", "注意", JOptionPane.ERROR_MESSAGE);
@@ -137,11 +134,15 @@ public class TransferCtrl {
                                 JOptionPane.showMessageDialog(null, "Dbf文件已生成", "注意", JOptionPane.PLAIN_MESSAGE);
                             }
                     }
-            }else{
-           JOptionPane.showMessageDialog(null, "请输入有效日期", "注意", JOptionPane.ERROR_MESSAGE);
             }   
+        }else{
+           JOptionPane.showMessageDialog(null, "请输入有效日期", "注意", JOptionPane.ERROR_MESSAGE);
+            }
+     
         }
-     }
+       }else{
+          JOptionPane.showMessageDialog(null, "请选择文件", "注意", JOptionPane.ERROR_MESSAGE);
+      }
     }
      
      private boolean isValidDate(String s){
@@ -155,7 +156,7 @@ public class TransferCtrl {
         }
      }
      
-     private boolean transferToTxt(String inputFilePath,String fileName,String outFileDir){
+     private boolean transferToTxt(String inputFilePath,String outFileDir){
          File inputFile = new File(inputFilePath);
          String inputFileName = inputFile.getName();
          String extension = inputFileName.lastIndexOf(".")==-1?"":inputFileName.substring(inputFileName.lastIndexOf(".")+1); 
@@ -170,8 +171,13 @@ public class TransferCtrl {
             int rsRows= rs.getRows();
             input = new StringBuffer();
             for( int i=1; i < rsRows; i++){
-                 String line  = "A999@" + rs.getCell(4, i).getContents() + "@" + rs.getCell(6,i).getContents() + "\r\n";
-                 input.append(line);
+                if(rs.getCell(5, i).getContents().equals("上期所")){
+                    String line  = "A999@" + rs.getCell(4, i).getContents() + "@" + rs.getCell(6,i).getContents() + "\r\n";
+                    input.append(line);
+                }else{
+                    continue;
+                }
+                
             }
          }catch (Exception e) {
             e.printStackTrace(); 
@@ -183,7 +189,6 @@ public class TransferCtrl {
             InputStream fs = new FileInputStream(inputFilePath);
             XSSFWorkbook wb;
              wb = new XSSFWorkbook(fs);
-             //wb = new XSSFWorkbook(inputFilePath);
              XSSFSheet sheet = wb.getSheetAt(0);
              int rows = sheet.getPhysicalNumberOfRows();
              input = new StringBuffer();
@@ -192,6 +197,7 @@ public class TransferCtrl {
                  if(row == null){
                      continue;
                  }
+                if(row.getCell(5).getStringCellValue().equals("上期所")){
                  if(row.getCell(4) == null){
                      row.createCell(4);
                      row.getCell(4).setCellValue("");
@@ -200,12 +206,27 @@ public class TransferCtrl {
                      row.createCell(6);
                      row.getCell(6).setCellValue("");
                  }
-                 row.getCell(4).setCellType(CELL_TYPE_STRING );
-                 row.getCell(6).setCellType(CELL_TYPE_STRING );
-                 String tradeCode = row.getCell(4).getStringCellValue();;
-                 String amount = row.getCell(6).getStringCellValue();
-                 String line = "A999@" + tradeCode + "@" + amount + "\r\n";
+
+                 String tradeCode = row.getCell(4).getStringCellValue();
+                 Double amount = null;
+                 String amt = null;  
+                 if(row.getCell(6).getCellType() == CELL_TYPE_NUMERIC){
+                     amount = row.getCell(6).getNumericCellValue() ;
+                     amt = new DecimalFormat("0.00").format(amount);       
+                 }else{
+                     if(row.getCell(6).getStringCellValue().length() == 0){
+                         amount = null;
+                         amt = "";
+                     }else{
+                     amount = new DecimalFormat("0.00").parse(row.getCell(6).getStringCellValue()).doubleValue();  //将String转换为Double
+                     amt = amount.toString();
+                     }
+                 }   
+                 String  line = "A999@" + tradeCode + "@" + amt + "\r\n";     
                  input.append(line);
+                }else{
+                    continue;
+                }
              }
          }catch (Exception e) {
              e.printStackTrace(); 
@@ -215,6 +236,7 @@ public class TransferCtrl {
          
          try {
             //创建输出文件
+            String fileName = "0004_00000001_" + frame.getDate().getText()+"_DailyFundChg";
             String outFile = outFileDir + "\\" + fileName + ".txt";
             File file = new File(outFile);
             if (!file.exists()) {
@@ -229,12 +251,13 @@ public class TransferCtrl {
             
         } catch (Exception e) {
            e.printStackTrace(); 
+           JOptionPane.showMessageDialog(null, "文件无法写入，可能另一个程序正在使用此文件", "注意", JOptionPane.ERROR_MESSAGE);
            return false;
         }
          
      }
      
-     private boolean transferToDbf(String inputFilePath,String fileName,String outFileDir) throws DBFException {
+     private boolean transferToDbf(String inputFilePath,String outFileDir) throws DBFException {
         
       //定义DBF文件字段,字段长度不确定
         DBFField[] fields = new DBFField[6];
@@ -257,7 +280,7 @@ public class TransferCtrl {
         fields[3] = new DBFField();
         fields[3].setName("AMOUNT");
         fields[3].setDataType(DBFField.FIELD_TYPE_N);
-        fields[3].setFieldLength(12);                         
+        fields[3].setFieldLength(23);                         
         fields[3].setDecimalCount(2);
              
         fields[4] = new DBFField();
@@ -268,7 +291,7 @@ public class TransferCtrl {
         fields[5] = new DBFField();
         fields[5].setName("TYPEMEMO");
         fields[5].setDataType(DBFField.FIELD_TYPE_C);
-        fields[5].setFieldLength(20);
+        fields[5].setFieldLength(40);
         DBFWriter writer = new DBFWriter();
         try{
          writer.setFields(fields);
@@ -289,23 +312,33 @@ public class TransferCtrl {
              jxl.Sheet rs = rwb.getSheet(0);
              int rsRows= rs.getRows();
              for( int i=1; i < rsRows; i++){
-                 Object[] rowData = new Object[6];
+                 if(rs.getCell(5, i).getContents().equals("中金所")){
+                     Object[] rowData = new Object[6];
                  rowData[0] = "000101";
                  rowData[1] = "0001";
                  rowData[4] = "A999";
                  String tradeCode = rs.getCell(4, i).getContents();
-                 double amount = 0;
+                 Double amount = null;
                  if(rs.getCell(6, i).getType() == CellType.NUMBER){
                      NumberCell numberCell = (NumberCell)rs.getCell(6, i); 
                      amount =numberCell.getValue();
                  }else{
-                     amount =new DecimalFormat("0.00").parse(rs.getCell(7,i).getContents()).doubleValue();    //将String转换为Double 
+                     if(rs.getCell(6,i).getContents().length() == 0){
+                         amount = null;
+                     }else{
+                         amount =new DecimalFormat("0.00").parse(rs.getCell(6,i).getContents()).doubleValue();    //将String转换为Double 
+                     }
+                    
                  }
                  String typeMemo = rs.getCell(7, i).getContents();
                  rowData[2] = tradeCode;
                  rowData[3] = amount;
                  rowData[5] = typeMemo;
                  writer.addRecord(rowData); 
+                 }else{
+                     continue;
+                 }
+                 
              }
           }catch (Exception e) {
              e.printStackTrace(); 
@@ -321,15 +354,17 @@ public class TransferCtrl {
                 XSSFSheet sheet = wb.getSheetAt(0);
                 int rows = sheet.getPhysicalNumberOfRows();
                 for(int i = 1;i<rows;i++){
-                    Object[] rowData = new Object[6];
-                    rowData[0] = "000101";
-                    rowData[1] = "0001";
-                    rowData[4] = "A999";
+                    
                     Row row = sheet.getRow(i);
                     if(row == null){
                         continue;
                     }
-                    if(row.getCell(4) == null){
+                  if(row.getCell(5).getStringCellValue().equals("中金所")){
+                        Object[] rowData = new Object[6];
+                        rowData[0] = "000101";
+                        rowData[1] = "0001";
+                        rowData[4] = "A999";
+                        if(row.getCell(4) == null){
                         row.createCell(4);
                         row.getCell(4).setCellValue("");
                     }
@@ -341,16 +376,27 @@ public class TransferCtrl {
                         row.createCell(7);
                         row.getCell(7).setCellValue("");
                     }
-                    row.getCell(4).setCellType(CELL_TYPE_STRING );
-                    row.getCell(6).setCellType(CELL_TYPE_NUMERIC );
-                    row.getCell(7).setCellType(CELL_TYPE_STRING);
-                    String tradeCode = row.getCell(4).getStringCellValue();;
-                    Double amount = row.getCell(6).getNumericCellValue();
+                    String tradeCode = row.getCell(4).getStringCellValue();
+                    Double amount = null;
+                    if(row.getCell(6).getCellType() == CELL_TYPE_NUMERIC){
+                         amount = row.getCell(6).getNumericCellValue();
+                    }else{
+                        row.getCell(6).setCellType(CELL_TYPE_STRING);
+                        if(row.getCell(6).getStringCellValue().length() == 0){
+                            amount = null;
+                        }else{
+                        amount = new DecimalFormat("0.00").parse(row.getCell(6).getStringCellValue()).doubleValue();
+                        }
+                    }
                     String typeMemo = row.getCell(7).getStringCellValue();
                     rowData[2] = tradeCode;
                     rowData[3] = amount;
                     rowData[5] = typeMemo;
                     writer.addRecord(rowData);
+                  }else{
+                        continue;
+                  }
+                    
                 }
             }catch (Exception e) {
                 e.printStackTrace(); 
@@ -360,18 +406,19 @@ public class TransferCtrl {
          
         try{        
               //创建输出文件
+            String fileName = "0001_SG01_" + frame.getDate().getText() + "_1_ClientCapitalDetail";
             String outFile = outFileDir + "\\" + fileName + ".dbf";
             File file = new File(outFile);
             if (!file.exists()) {
                 file.createNewFile();
             }
              OutputStream os = new FileOutputStream(file);
-
-             //写入数据 
-            writer.write(os); 
+            writer.write(os);    //写入数据 
+            os.close();
             return true;
         }catch(Exception e){
             e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "文件无法写入，可能另一个程序正在使用此文件", "注意", JOptionPane.ERROR_MESSAGE);
             return false;
         }     
      }
--- ExcelTransfer/src/com/gtja/qh/View/Frame.java
+++ ExcelTransfer/src/com/gtja/qh/View/Frame.java
@@ -2,29 +2,35 @@ package com.gtja.qh.View;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 /**
  *
  * @author Administrator
  */
-public class Frame extends javax.swing.JFrame {
+public class Frame extends JFrame {
 
     private javax.swing.JButton selectFileButton;
     private javax.swing.JButton generateButton;
     private javax.swing.JCheckBox TxtCB;
-    private javax.swing.JCheckBox DbfCB;
+    private javax.swing.JCheckBox DbfCB;   
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JTextField filePath;
     private javax.swing.JTextField date;
     
     public Frame() {
         initComponents();
         this.setSizeAndCentralizeMe(450, 300);
+        this.setTitle("Excel文件转换工具(结息专用）");
+        this.getRootPane().setDefaultButton(generateButton);
         this.setVisible(true);
     }
 
@@ -32,13 +38,19 @@ public class Frame extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
-
+         String windows="com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
+        try {
+            UIManager.setLookAndFeel(windows);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
         filePath = new javax.swing.JTextField();
         selectFileButton= new javax.swing.JButton();
         TxtCB = new javax.swing.JCheckBox();
         DbfCB = new javax.swing.JCheckBox();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         date = new javax.swing.JTextField();
         generateButton = new javax.swing.JButton();
 
@@ -53,10 +65,10 @@ public class Frame extends javax.swing.JFrame {
 //            }
 //        });
 
-        TxtCB.setText("TXT");
+        TxtCB.setText("TXT(上期所）");
         TxtCB.setSelected(true);
 
-        DbfCB.setText("DBF");
+        DbfCB.setText("DBF（中金所）");
         DbfCB.setSelected(true);
 //        DbfCB.addActionListener(new java.awt.event.ActionListener() {
 //            public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -64,65 +76,69 @@ public class Frame extends javax.swing.JFrame {
 //            }
 //        });
 
-        jLabel1.setText("输出文件类型");
+        jLabel1.setText("源文件：");
+        
+        jLabel2.setText("输出文件类型：");
 
-        jLabel2.setText("日期（YYYYMMDD）：");
+        jLabel3.setText("日期（YYYYMMDD）：");
 
         //date.setText("date");
+        Date dt = new Date();
+        SimpleDateFormat sdf =new SimpleDateFormat("yyyyMMdd");
+        date.setText(sdf.format(dt));
 
         generateButton.setText("生成");
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(50, 50, 50)
+                .addGap(20, 20, 20)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(TxtCB)
-                        .addGap(45, 45, 45)
-                        .addComponent(DbfCB))
-                    .addComponent(filePath, javax.swing.GroupLayout.PREFERRED_SIZE, 242, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(filePath, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(selectFileButton))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(generateButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(selectFileButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(29, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addComponent(TxtCB)
+                        .addGap(18, 18, 18)
+                        .addComponent(DbfCB))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(33, 33, 33)
+                        .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(generateButton, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(40, 40, 40)
+                .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
                     .addComponent(filePath, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(selectFileButton))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(TxtCB)
-                            .addComponent(DbfCB)
-                            .addComponent(jLabel1))
-                        .addGap(31, 31, 31)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(generateButton, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(39, Short.MAX_VALUE))
+                .addGap(42, 42, 42)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(TxtCB)
+                    .addComponent(DbfCB))
+                .addGap(42, 42, 42)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(generateButton, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
-    }// </editor-fold>                        
-
+    }
 //    private void selectFileButtonActionPerformed(java.awt.event.ActionEvent evt) {                                         
 //        // TODO add your handling code here:
 //    }                                        
